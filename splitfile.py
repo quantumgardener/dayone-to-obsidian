2b4ecf9f8173817518f3743a0922b8ddd88653e5
David Buchan <david@buchan.org>
2021-02-21 10:20:13 +1100

    Basic load in place

--- splitfile.py
+++ splitfile.py
@@ -1,10 +1,13 @@
 import datetime
 import dateutil.parser
 import json
-import pytz
+import pytz    # pip install pytz
 import re
+import os
 
-fn = "D:\OneDrive\Documents\dayone\Journal.json"
+# Set this as the location where your Journal.json file is located
+root = r"D:\OneDrive\Documents\dayone" 
+fn = os.path.join( root, "Journal.json" )
 with open(fn, encoding='utf-8') as json_file:
     data = json.load(json_file)
     for entry in data['entries']:
@@ -41,28 +44,64 @@ with open(fn, encoding='utf-8') as json_file:
             newText = entry['text'].replace("\\", "")
             newText = newText.replace( "\u2028", "\n")
             newText = newText.replace( "\u1C6A", "\n\n")
-            #newText = newText.replace( "![]dayone-moment://", "photos/")
-            # Replace all photos
-            #newText = re.search("(\!\[\]\(dayone-moment:\/\/)([A-F0-9]+)(\))", newText)
-            newText = re.sub("(\!\[\]\(dayone-moment:\/\/)([A-F0-9]+)(\))", r'[[\2.jpeg]]', newText)
+
+            if 'photos' in entry:
+                # Correct photo links. First we need to rename them. The filename is the md5 code, not the identifier
+                # subsequently used in the text. Then we can amend the text to match. Will only to rename on first run
+                # through as then, they are all renamed.
+                # Assuming all jpeg extensions.
+
+                for p in entry['photos']:
+                    pfn = os.path.join( root, 'photos', '%s.jpeg' % p['md5'] )
+                    if os.path.isfile( pfn ):
+                        newfn = os.path.join( root, 'photos', '%s.jpeg' % p['identifier'] )
+                        print ( 'Renaming %s to %s' % (pfn, newfn ))
+                        os.rename( pfn, newfn )
+
+                # Now to replace the text to point to the file in obsidian
+                newText = re.sub(r"(\!\[\]\(dayone-moment:\/\/)([A-F0-9]+)(\))", r'![[\2.jpeg]]', newText)
+
             newEntry.append( newText )
         except KeyError:
             pass
 
         # Let's add some tags at the bottom to finish up
-        tags = ''
+        tags = '#journal/entry\n'
         if 'tags' in entry:
             for t in entry['tags']:
-                tags = "%s #%s" % (tags, t.replace(' ', '-'))
+                tags = "%s#journal/%s\n" % (tags, t.replace(' ', '-'))
 
         if entry['starred']:
-            tags = "%s #starred" % tags 
+            tags = "%s#journal/starred\n" % tags 
 
-        if tags != '':
-            newEntry.append( "\n\n%s" % tags )
+        newEntry.append( "\n\n%s" % tags )
+
+        # Save entries organised by year, year-month, year-month-day.md
+        yearDir = os.path.join( root, str(createDate.year) )
+        monthDir = os.path.join( yearDir, createDate.strftime( '%Y-%m'))
+        try:
+            os.stat(yearDir)
+        except:
+            os.mkdir(yearDir)
+        
+        try:
+            os.stat(monthDir)
+        except:
+            os.mkdir(monthDir)
+        
+        # Target filename to save to. Will be modified if already exists
+        fnNew = os.path.join( monthDir, "%s.md" % createDate.strftime( '%Y-%m-%d'))
+        
+        # Here is where we handle multiple entries on the same day. Each goes to it's own file
+        if os.path.isfile( fnNew):
+            # File exists, need to find the next in sequence
+            index = 97 #ASCII a
+            fnNew = os.path.join( monthDir, "%s-%s.md" % (createDate.strftime( '%Y-%m-%d'), chr(index)))
+            while os.path.isfile(fnNew):
+                index += 1
+                fnNew = os.path.join( monthDir, "%s-%s.md" % (createDate.strftime( '%Y-%m-%d'), chr(index)))
 
 
-        fnNew = "D:\OneDrive\Documents\dayone\%s.md" % createDate.strftime( '%Y-%m-%d')
         with open(fnNew, 'w', encoding='utf-8') as f:
          for line in newEntry:
             f.write(line)
